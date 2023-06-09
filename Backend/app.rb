566c3dec08e969564849a8d02c7054f04d860b3d
allenamaya <ahlee.amaya@gmail.com>
2023-06-09 13:03:28 +0300

    https://github.com/allenamaya/ALISSO-STUDENT-MANAGEMENT-SYSTEM/tree/Backend

---
+++ Backend/app.rb
@@ -0,0 +1,109 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'bcrypt'
+
+# Configuration for SQLite3 database
+set :database, { adapter: 'sqlite3', database: 'student_management.db' }
+
+# Models
+require './models/student'
+require './models/teacher'
+require './models/grade'
+require './models/attendance'
+require './models/assignment'
+
+# Enable sessions for user authentication
+enable :sessions
+
+# Helper method to check if user is logged in
+def logged_in?
+  !session[:user_id].nil?
+end
+
+# Routes
+
+# Main route
+get '/' do
+  erb :index
+end
+
+# Authentication routes
+
+# Student login
+get '/student/login' do
+  erb :'student/login'
+end
+
+post '/student/login' do
+  student = Student.find_by(email: params[:email])
+  if student && BCrypt::Password.new(student.password) == params[:password]
+    session[:user_id] = student.id
+    redirect '/student/dashboard'
+  else
+    @error = 'Invalid email or password'
+    erb :'student/login'
+  end
+end
+
+# Teacher login
+get '/teacher/login' do
+  erb :'teacher/login'
+end
+
+post '/teacher/login' do
+  teacher = Teacher.find_by(email: params[:email])
+  if teacher && BCrypt::Password.new(teacher.password) == params[:password]
+    session[:user_id] = teacher.id
+    redirect '/teacher/dashboard'
+  else
+    @error = 'Invalid email or password'
+    erb :'teacher/login'
+  end
+end
+
+# Logout
+post '/users/signout' do
+  session.clear
+  redirect '/'
+end
+
+# Student routes
+
+# Student dashboard
+get '/student/dashboard' do
+  redirect '/student/login' unless logged_in?
+  # Render the student dashboard
+  erb :'student/dashboard'
+end
+
+# Student profile
+get '/student/profile' do
+  redirect '/student/login' unless logged_in?
+  # Retrieve and render the student's profile
+  @student = Student.find(session[:user_id])
+  erb :'student/profile'
+end
+
+# Teacher routes
+
+# Teacher dashboard
+get '/teacher/dashboard' do
+  redirect '/teacher/login' unless logged_in?
+  # Render the teacher dashboard
+  erb :'teacher/dashboard'
+end
+
+# Teacher profile
+get '/teacher/profile' do
+  redirect '/teacher/login' unless logged_in?
+  # Retrieve and render the teacher's profile
+  @teacher = Teacher.find(session[:user_id])
+  erb :'teacher/profile'
+end
+
+# Error handling
+
+# 404 Not Found
+not_found do
+  erb :'errors/404'
+end
--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'activerecord'
 gem 'sqlite3'
+gem 'sinatra-activerecord'
 # gem "rails"
